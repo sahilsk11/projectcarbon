fdcbdcaa59609a004a4d1e21039735d3d7606ff5
Sahil Kapur <sahilkapur.a@gmail.com>
2019-04-11 01:19:02 -0400

    refactored response handler

--- devices/co2/server-side/response_handler.py
+++ devices/co2/server-side/response_handler.py
@@ -19,7 +19,7 @@ def parse_csv(values):
 
     parsed_data = raw_data.split("\n")
     if (values > len(parsed_data)):
-        values = len(parsed_data)
+        values = len(parsed_data    )
     for i in range(len(parsed_data) - values, len(parsed_data)):
         parsed_data[i] = parsed_data[i].split(",")
         time_values.append(parsed_data[i][0])
@@ -27,12 +27,27 @@ def parse_csv(values):
     #print(time_values)
     return [co2_levels, time_values]
 
+def insert_value(co2, time):
+    f = open(file_handle, "a")
+    f.write(time + "," + co2)
+    f.close()
+    sucess = True
+    return success
+
 form = cgi.FieldStorage()
 command = form.getfirst("command", "")
 values = form.getfirst("values", "")
+co2 = form.getfirst("co2")
+time = form.getfirst("time")
 
 data = parse_csv(int(values))
 
 if (command == "getchart"):
     j = json.dumps(data)
+    print(j)
+
+elif (command == "insert"):
+    success = insert_value(co2, time)
+    d = {"success": success}
+    j = json.dumps(d)
     print(j)
