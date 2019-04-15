1e507b1b899ee2b18494c4ec2ba1067cfe33efa0
Sahil Kapur <sahilkapur.a@gmail.com>
2019-04-14 22:06:34 -0400

    Changed fonts and added normal value chart data

--- devices/server-side/response_handler.py
+++ devices/server-side/response_handler.py
@@ -2,6 +2,7 @@
 
 import cgi
 import json
+import datetime
 
 print("Content-type: application/json\n\n")
 
@@ -23,10 +24,18 @@ def parse_csv(values):
         values += 1
     if (values > len(parsed_data)):
         values = len(parsed_data)
+
+    normal_co2 = 0
+    high_co2 = 0
+    normal_temp = 0
+    low_temp = 0
+    high_temp = 0
     for i in range(len(parsed_data) - values, len(parsed_data)):
         if (parsed_data[i] != ''):
             parsed_data[i] = parsed_data[i].split(",")
             time_stamp = parsed_data[i][0]
+            #parsed_time = datetime.datetime.strptime(time_stamp, "%Y-%m-")
+            #formatted_time = parsed_time.strftime("%-I:%M")
             time_values.append(time_stamp)
 
             co2_reading = parsed_data[i][1]
@@ -35,6 +44,10 @@ def parse_csv(values):
                 co2_reading = None
             else:
                 co2_reading = int(co2_reading)
+                if (co2_reading > 1000):
+                    high_co2 += 1
+                else:
+                    normal_co2 += 1
             co2_levels.append(co2_reading)
 
             temperature = parsed_data[i][2]
@@ -42,10 +55,16 @@ def parse_csv(values):
                 temperature = None
             else:
                 temperature = int(temperature)
+                if (temperature > 77):
+                    high_temp += 1
+                elif (temperature < 68):
+                    low_temp +=1
+                else:
+                    normal_temp += 1
             temp_levels.append(temperature)
 
     #print(time_values)
-    return [time_values, co2_levels, temp_levels]
+    return [time_values, co2_levels, temp_levels, normal_co2, high_co2, normal_temp, low_temp, high_temp]
 
 def insert_value(time, co2, temperature):
     success = "true"
@@ -64,7 +83,6 @@ values = form.getfirst("values", "")
 co2 = form.getfirst("co2")
 time = form.getfirst("time")
 temperature = form.getfirst("temperature")
-
 
 if (command == "getchart"):
     data = parse_csv(int(values))
